318465fc703c0a60dac228f5d60059d542c9a86f
NewName4ME <tortarod@gmail.com>
2024-09-24 17:27:13 +0200

    ajustest finales para push

--- Cliente/T1/R4/controlador.js
+++ Cliente/T1/R4/controlador.js
@@ -27,6 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const SECTION = document.querySelector('SECTION');
             SECTION.innerHTML = '';
 
+            //Objeto que relaciona cada boton con el metodo que debe ejecutar
             const funcionesQueDebeMostrarCadaBoton = {
                 "btn1": sum,
                 "btn2": sumOfAll,
@@ -43,6 +44,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 "btn13": calularFactorial,
             }
 
+            //esto deberia haberlo movido a un JSON y hacer un fetch
+            //pero no queria darle más vueltas
             const testDeCadaFuncion = {
                 "btn1": {
                     "name": "sum",
@@ -63,6 +66,77 @@ document.addEventListener('DOMContentLoaded', () => {
                     "tests": [
                         { input: [2, 6, 7, "holiwi", "5"] },
                         { input: ["0", true, 5] }
+                    ]
+                },
+                "btn4": {
+                    "name": "combineTwoArrays",
+                    "tests": [
+                        { input: [[1, 1, 1, 1], [2, 2, 2, 2]] },
+                        { input: [[1, 1, 1, "b"], ["a", 2, 2]] }
+                    ]
+                },
+                "btn5": {
+                    "name": "sumEveryOther",
+                    "tests": [
+                        { input: [1, 4, 2, 4] },
+                        { input: [1, 4, 2, 4, 4] }
+                    ]
+                },
+                "btn6": {
+                    "name": "divisible",
+                    "tests": [
+                        { input: [6] },
+                        { input: [7] }
+                    ]
+                },
+                "btn7": {
+                    "name": "divisibleEntre",
+                    "tests": [
+                        { input: [6, 2] },
+                        { input: [7, 3] }
+                    ]
+                },
+                "btn8": {
+                    "name": "rango",
+                    "tests": [
+                        { input: [6, 2, 9] },
+                        { input: [2, 0, 1] }
+                    ]
+                },
+                "btn9": {
+                    "name": "tieneTresDigitos",
+                    "tests": [
+                        { input: [620] },
+                        { input: [2] }
+                    ]
+                },
+                "btn10": {
+                    "name": "areaRectangulo",
+                    "tests": [
+                        { input: [20, 2] },
+                        { input: [5, 1] }
+                    ]
+                },
+                "btn11": {
+                    "name": "imc",
+                    "tests": [
+                        { input: [70, 1.82] },
+                        { input: [45, 1.54] }
+                    ]
+                },
+                "btn12": {
+                    "name": "precioFinal",
+                    "tests": [
+                        { input: [100, 20] },
+                        { input: [50, 50] }
+                    ]
+                },
+                "btn13": {
+                    "name": "calularFactorial",
+                    "tests": [
+                        { input: [5] },
+                        { input: [170] },
+                        { input: [171] }
                     ]
                 }
             }
@@ -89,12 +163,12 @@ document.addEventListener('DOMContentLoaded', () => {
         tests.forEach(test => {
             const { input } = test;
             const BUTTON = document.createElement('BUTTON');
-
             BUTTON.textContent = `${name}(${input})`;
 
             //añadimos a cada boton un evento que ejecute la funcion necesaria
             BUTTON.onclick = () => {
                 const result = funcionAEjecutar(...input);
+                console.log(result);
                 alert(result);
             };
 
