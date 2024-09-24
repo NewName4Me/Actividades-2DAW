f7ee03e9e200359d66681d28751705ae31dbf923
NewName4ME <tortarod@gmail.com>
2024-09-24 17:05:02 +0200

    crear funcion que ejecuta los tests

--- Cliente/T1/R4/controlador.js
+++ Cliente/T1/R4/controlador.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const SECTION = document.querySelector('SECTION');
             SECTION.innerHTML = '';
 
-            const funcioensQueDebeMostrarCadaBoton = {
+            const funcionesQueDebeMostrarCadaBoton = {
                 "btn1": sum,
                 "btn2": sumOfAll,
                 "btn3": countTheArgs,
@@ -43,9 +43,64 @@ document.addEventListener('DOMContentLoaded', () => {
                 "btn13": calularFactorial,
             }
 
+            const testDeCadaFuncion = {
+                "btn1": {
+                    "name": "sum",
+                    "tests": [
+                        { input: [2, 6, 7] },
+                        { input: [2, 6, -1] }
+                    ]
+                },
+                "btn2": {
+                    "name": "sumOfAll",
+                    "tests": [
+                        { input: [2, 6, 7, "hola"] },
+                        { input: [2, 6, "testin", -1] }
+                    ]
+                },
+                "btn3": {
+                    "name": "countTheArgs",
+                    "tests": [
+                        { input: [2, 6, 7, "holiwi", "5"] },
+                        { input: ["0", true, 5] }
+                    ]
+                }
+            }
+
             //ejecutamos la funcion correspondiente
-            PRE.textContent = funcioensQueDebeMostrarCadaBoton[BTN_ID].toString();
+            PRE.textContent = funcionesQueDebeMostrarCadaBoton[BTN_ID].toString();
+
+            addButtonsFOrTest(testDeCadaFuncion[BTN_ID], funcionesQueDebeMostrarCadaBoton[BTN_ID]);
 
         });
     });
+
+    /**
+     * funcion que ejecuta añade los botones al DOM con las propiedades necesarias para poder ejecutar dichos metodos
+     * @param {*} funcionesTests 
+     * @param {*} funcionAEjecutar le pasamos una funcion para asi poderle dar el atributo al boton para ejecutarla
+     */
+    function addButtonsFOrTest(funcionesTests, funcionAEjecutar) {
+        const H3 = document.querySelector('h3');
+
+        const { name, tests } = funcionesTests;
+
+        //recorremos todos los tests que tenemos almacenados
+        tests.forEach(test => {
+            const { input } = test;
+            const BUTTON = document.createElement('BUTTON');
+
+            BUTTON.textContent = `${name}(${input})`;
+
+            //añadimos a cada boton un evento que ejecute la funcion necesaria
+            BUTTON.onclick = () => {
+                const result = funcionAEjecutar(...input);
+                alert(result);
+            };
+
+            H3.append(BUTTON);
+        });
+
+    }
+
 });
