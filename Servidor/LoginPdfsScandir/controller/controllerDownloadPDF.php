e2eb7bea3a9eb958f3219958dedd422f562ad8e2
NewName4ME <tortarod@gmail.com>
2024-10-21 12:34:05 +0200

    añadir doc

--- Servidor/LoginPdfsScandir/controller/controllerDownloadPDF.php
+++ Servidor/LoginPdfsScandir/controller/controllerDownloadPDF.php
@@ -12,6 +12,9 @@ if (isset($descargarPDF)) {
     descargarPDF();
 }
 
+/**
+ * The function `descargarPDF` generates a PDF file from HTML content and streams it for download.
+ */
 function descargarPDF()
 {
     global $rutaCompleta;
@@ -23,6 +26,17 @@ function descargarPDF()
     $dompdf->render();
     $dompdf->stream($rutaCompleta . '.pdf');
 }
+
+/**
+ * The function "tomarTextoDelArchivo" reads the contents of a file and returns it as a string.
+ * 
+ * @param file The `tomarTextoDelArchivo` function takes a file path as a parameter and reads the
+ * contents of the file using `file_get_contents`. The parameter `` should be a string
+ * representing the path to the file from which you want to read the text.
+ * 
+ * @return The function `tomarTextoDelArchivo()` is returning the contents of the file specified
+ * by the `` parameter using the `file_get_contents()` function.
+ */
 
 function tomarTextoDelArchivo($file)
 {
